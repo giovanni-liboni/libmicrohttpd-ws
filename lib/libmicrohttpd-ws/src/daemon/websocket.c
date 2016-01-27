8dce72f9249e4f030b6dadfeefd0b09647d6c3c0
Giovanni Liboni <giovanni.liboni@live.com>
2016-01-27 12:25:15 +0100

    License update: released under Apache License 2.0

--- lib/libmicrohttpd-ws/src/daemon/websocket.c
+++ lib/libmicrohttpd-ws/src/daemon/websocket.c
@@ -1,6 +1,6 @@
 /*
-     This file is part of libuhttpd-ws
-     (C) 2013 Giovanni Liboni
+     This file is part of libmicrohttpd
+     (C) 2013 2014 2015 2016 Giovanni Liboni
 
      This library is free software; you can redistribute it and/or
      modify it under the terms of the GNU Lesser General Public
@@ -15,11 +15,26 @@
      You should have received a copy of the GNU Lesser General Public
      License along with this library; if not, write to the Free Software
      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+*/
 /*
- * websocket.c
- *      Author: Giovanni Liboni
- */
+ 		Copyright 2016 Giovanni Liboni
+
+ 		Licensed under the Apache License, Version 2.0 (the "License");
+ 		you may not use this file except in compliance with the License.
+ 		You may obtain a copy of the License at
+
+ 				http://www.apache.org/licenses/LICENSE-2.0
+
+ 		Unless required by applicable law or agreed to in writing, software
+ 		distributed under the License is distributed on an "AS IS" BASIS,
+ 		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ 		See the License for the specific language governing permissions and
+ 		limitations under the License.
+*/
+/*
+* @file websocket.c
+* @author Giovanni Liboni
+*/
 
 #include "websocket.h"
 #include <sys/socket.h>
